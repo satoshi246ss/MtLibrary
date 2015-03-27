4c6965dec1d058ff818beb6494b098eaac1ed5cf
satoshi <satoshi246ss@yahoo.co.jp>
2015-03-27 13:21:45 +0900

    z_correct

---
+++ Z_correction_test/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Z_correction_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox_MT2Az = new System.Windows.Forms.TextBox();
+            this.textBox_MT2Alt = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(-3, 164);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(284, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox_MT2Az
+            // 
+            this.textBox_MT2Az.Location = new System.Drawing.Point(31, 69);
+            this.textBox_MT2Az.Name = "textBox_MT2Az";
+            this.textBox_MT2Az.Size = new System.Drawing.Size(58, 19);
+            this.textBox_MT2Az.TabIndex = 2;
+            this.textBox_MT2Az.Text = "90";
+            // 
+            // textBox_MT2Alt
+            // 
+            this.textBox_MT2Alt.Location = new System.Drawing.Point(31, 94);
+            this.textBox_MT2Alt.Name = "textBox_MT2Alt";
+            this.textBox_MT2Alt.Size = new System.Drawing.Size(58, 19);
+            this.textBox_MT2Alt.TabIndex = 3;
+            this.textBox_MT2Alt.Text = "80";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "mmWest",
+            "mmEast"});
+            this.comboBox1.Location = new System.Drawing.Point(31, 119);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox_MT2Alt);
+            this.Controls.Add(this.textBox_MT2Az);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox_MT2Az;
+        private System.Windows.Forms.TextBox textBox_MT2Alt;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Z_correction_test/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+
+namespace Z_correction_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 地平座標->方向余弦
+        /// </summary>
+        public Vector eq_directional_cosine(double az, double alt)
+        {
+            var v = Vector.Build.Dense(3);
+            //DenseVector ve = ve.Dense(10);
+            const double RAD = Math.PI / 180.0;
+
+            // 地平座標の方向余弦
+            v[0] = Math.Cos(alt * RAD) * Math.Cos(az * RAD);
+            v[1] = -Math.Cos(alt * RAD) * Math.Sin(az * RAD);
+            v[2] = Math.Sin(alt * RAD);
+
+            return (Vector)v;
+        }
+        /// <summary>
+        /// 地平座標<-方向余弦
+        /// </summary>
+        public void eq_rev_directional_cosine(Vector v, out double az, out double alt)
+        {
+            const double RAD = Math.PI / 180.0;
+
+            alt = Math.Asin(v[2]) / RAD;
+            az = 0;
+            if (Math.Abs(v[0]) < 1e-9)
+            {
+                if (-v[1] >= 0) az = 90;
+                if (-v[1] < 0) az = -90;
+            }
+            else
+            {
+                az = Math.Atan2(-v[1], v[0]) / RAD;
+            }
+
+            while (az < 0) az += 360;
+            while (az >= 360) az -= 360;
+        }
+        /// <summary>
+        /// X軸回転
+        /// </summary>
+        public Matrix Rotate_X(double theta)
+        {
+            var m = Matrix.Build.Dense(3, 3);
+            const double RAD = Math.PI / 180.0;
+            double sinth = Math.Sin(theta * RAD);
+            double costh = Math.Cos(theta * RAD);
+
+            m[0, 0] = 1;
+            m[0, 1] = 0;
+            m[0, 2] = 0;
+
+            m[1, 0] = 0;
+            m[1, 1] = costh;
+            m[1, 2] = -sinth;
+
+            m[2, 0] = 0;
+            m[2, 1] = sinth;
+            m[2, 2] = costh;
+
+            return (Matrix)m;
+        }
+        /// <summary>
+        /// Y軸回転
+        /// </summary>
+        public Matrix Rotate_Y(double theta)
+        {
+            var m = Matrix.Build.Dense(3, 3);
+            const double RAD = Math.PI / 180.0;
+            double sinth = Math.Sin(theta * RAD);
+            double costh = Math.Cos(theta * RAD);
+
+            m[0, 0] = costh;
+            m[0, 1] = 0;
+            m[0, 2] = sinth;
+
+            m[1, 0] = 0;
+            m[1, 1] = 1;
+            m[1, 2] = 0;
+
+            m[2, 0] = -sinth;
+            m[2, 1] = 0;
+            m[2, 2] = costh;
+
+            return (Matrix)m;
+        }
+        /// <summary>
+        /// Z軸回転
+        /// </summary>
+        public Matrix Rotate_Z(double theta)
+        {
+            var m = Matrix.Build.Dense(3, 3);
+            const double RAD = Math.PI / 180.0;
+            double sinth = Math.Sin(theta * RAD);
+            double costh = Math.Cos(theta * RAD);
+
+            m[0, 0] = costh;
+            m[0, 1] = -sinth;
+            m[0, 2] = 0;
+
+            m[1, 0] = sinth;
+            m[1, 1] = costh;
+            m[1, 2] = 0;
+
+            m[2, 0] = 0;
+            m[2, 1] = 0;
+            m[2, 2] = 1;
+
+            return (Matrix)m;
+        }
+        /// <summary>
+        /// Az軸方向と天頂との誤差の補正
+        /// Zaz：天頂から見たAz軸方位(deg)
+        /// dat：天頂から見たAz軸距離(deg)
+        /// </summary>
+        public void z_correct(double az, double alt, double zaz, double dzt, out double az_zc, out double alt_zc)
+        {
+            Vector vt = eq_directional_cosine(az, alt);
+
+            Matrix my = Rotate_Y(-dzt); // 天頂づれ補正　dzt:北側が＋
+            Matrix mz1 = Rotate_Z(zaz); // 
+            Matrix mz2 = Rotate_Z(-zaz); // 
+
+            var v1 = mz1.Multiply(vt);
+            eq_rev_directional_cosine((Vector)v1, out az_zc, out alt_zc);
+            var v2 = my.Multiply(v1);
+            eq_rev_directional_cosine((Vector)v2, out az_zc, out alt_zc);
+            var v3 = mz2.Multiply(v2);
+            eq_rev_directional_cosine((Vector)v3, out az_zc, out alt_zc);
+        }
+        /// <summary>
+        /// Az軸とAlt軸の直交誤差の補正
+        /// </summary>
+        public void azalt_correct(double az, double alt, double zaz, double dzt, out double az_zc, out double alt_zc)
+        {
+            Vector vt = eq_directional_cosine(az, alt);
+
+            Matrix my = Rotate_Y(-dzt); // 天頂づれ補正　dzt:北側が＋
+            Matrix mz1 = Rotate_Z( zaz); // 
+            Matrix mz2 = Rotate_Z( -zaz); // 
+            if (comboBox1.Text == "mmEast")
+            {
+                //my  = Rotate_Y(-dzt); // 天頂づれ補正　dzt:北側が＋
+              //  mz1 = Rotate_Z(90  - az - zaz); // 
+              //  mz2 = Rotate_Z(-(90  - az - zaz)); // 
+            }
+            var v1 = mz1.Multiply(vt);
+            eq_rev_directional_cosine((Vector)v1, out az_zc, out alt_zc);
+            var v2 = my.Multiply(v1);
+            eq_rev_directional_cosine((Vector)v2, out az_zc, out alt_zc);
+            var v3 = mz2.Multiply(v2);
+
+            eq_rev_directional_cosine((Vector)v3, out az_zc, out alt_zc);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double mt2az = Convert.ToDouble(textBox_MT2Az.Text);
+           double mt2alt = Convert.ToDouble(textBox_MT2Alt.Text);
+           double mt2zaz = 185; // Convert.ToDouble(textBox_MT2ZAz.Text);
+           double mt2zdt = 2.0;// Convert.ToDouble(textBox_MT2ｄZT.Text);
+
+            double az_zc, alt_zc;
+            azalt_correct(mt2az, mt2alt, mt2zaz, mt2zdt, out az_zc, out alt_zc);
+            string s = string.Format("Az:{0},{1}  {2},{3}  ans:{4,0:F1},  {5,0:F1}\n", mt2az, mt2alt, mt2zaz, mt2zdt, az_zc, alt_zc);
+            richTextBox1.AppendText(s);
+
+            mt2zaz = 130; // Convert.ToDouble(textBox_MT2ZAz.Text);
+            mt2zdt = 7.6;// Convert.ToDouble(textBox_MT2ｄZT.Text);
+            z_correct(mt2az, mt2alt, mt2zaz, mt2zdt, out az_zc, out alt_zc);
+            s = string.Format("Az:{0},{1}  {2},{3}  ans:{4,0:F1},  {5,0:F1}\n", mt2az, mt2alt, mt2zaz, mt2zdt, az_zc, alt_zc);
+            richTextBox1.AppendText(s);
+
+        }
+    }
+}
